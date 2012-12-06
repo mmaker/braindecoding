e8966d98254ad8f84dafc546e8f19d6e5d10c642
Michele Orru` <maker@python.it>
2012-12-07 00:03:39 +0100

    Adding preprossing file

---
+++ fuffa.py
@@ -0,0 +1,56 @@
+#coding: utf-8
+from __future__ import division
+import os.path
+
+import numpy as np
+import pylab as pl
+from sklearn import svm
+"""
+fsample --> si riferisce solamente alla frequenza di campionamento (300 Hz);
+label --> associa ad ogni canale un'etichetta per identificarlo spazialmente:
+'MLx' (left), 'MRx' (right) e 'MZx' (zenith) con x=C,F,O,P,T per central,
+frontal, occipital, parietal and temporal;
+lf_freq --> matrice [127 x 274 x 376] = [n trial con task left x n canali x
+densità spettrale] (matrice che più ti interessa, da questa ti ricavi la densità
+media nei vari intervalli: 2-4 4-8 8-13 ecc);
+sample_freq --> asse delle frequenze (in Hz) su cui è calcolata la densità
+spettrale di potenza;
+time --> asse dei tempi (in secondi) su cui era acquisito il segnale meg.
+"""
+
+def preprocess():
+    _curdir = os.path.dirname(__file__)
+    left = np.load(os.path.join(_curdir, 'freq_left_gradient.npy.npz'))
+    right = np.load(os.path.join(_curdir, 'freq_right_gradient.npy.npz'))
+
+    dataset = dict()
+    for direction, trials in ('left', left['lf_freq']), ('right', right['rg_freq']):
+        samples, channels, freqs = trials.shape
+        step = freqs // 6
+        features = [[[np.mean(trials[trial][channel][start:start+step])
+                     for start in xrange(0, freqs, step)]
+                    for channel in xrange(channels)]
+                   for trial in xrange(samples)]
+        dataset[direction] = features
+
+    np.savez_compressed(os.path.join(_curdir, 'dataset'), **dataset)
+
+
+# set up first classifier
+
+# set up second classifier
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Machine Learning MEG project')
+    parser.add_argument('-p', '--preprocess',
+                        action='store_true',
+                        dest='preprocess',
+                        help='Preprocess datased given by CIMeC',
+    )
+    args = parser.parse_args()
+    if args.preprocess:
+        preprocess()
+    else:
+        parser.print_help()
