3506fa51540cfec21701e52c8fb5b5580b9fba79
Michele Orru` <maker@python.it>
2012-12-14 21:31:39 +0100

    metadata

--- braindecoding.py
+++ braindecoding.py
@@ -11,6 +11,25 @@ import numpy as np
 import pylab as pl
 from sklearn import svm, cross_validation
 
+
+__author__ = 'Michele Orru`'
+__email__ = 'maker@python.it'
+__license__ = """ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                   Version 2, December 2004
+
+Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+ 0. You just DO WHAT THE FUCK YOU WANT TO.
+"""
+
+
 _curdir = os.path.dirname(__file__)
 
 # hide shitty gtk+ warning messages.
